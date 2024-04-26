03b1d4ba908d7be2b0511089437e0b3f92a85af8
Primexz <johann@pepebot.info>
2024-04-26 18:40:06 +0200

    feat: add versioning to binary

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"log"
+	"runtime"
 
 	"github.com/primexz/KrakenDCA/bot"
 	"github.com/primexz/KrakenDCA/config"
 )
 
 func main() {
-	log.Println("Starting Kraken DCA Bot")
+	log.Printf("Kraken DCA %s, commit %s, built at %s", version, commit, date)
+	log.Printf("%s [%s,%s]", runtime.Version(), runtime.GOOS, runtime.GOARCH)
 
 	config.LoadConfiguration()
 	bot.StartBot()
